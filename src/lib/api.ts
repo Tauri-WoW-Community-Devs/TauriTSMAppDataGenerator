47a408d11cb3207e9fb6d3e45df4951e262247e3
Łukasz K <maczugapl@gmail.com>
2022-01-18 11:37:32 +0100

    Handle secret the new way as well.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,15 +3,6 @@ import {Realm,RealmXSource,ApiSourceInfo} from "src/constants/realms";
 
 export const apiCall = async <T>(url: string, realm: Realm, params?: { [key: string]: any }) => {
   try {
-    const data = {
-      secret: realm === Realm.MISTBLADE ? process.env.STORMFORGE_SECRET_KEY : process.env.SECRET_KEY,
-      url,
-      params: {
-        r: realm,
-        ...params,
-      }
-    };
-
     const config: AxiosRequestConfig = {
       headers: {
         "Content-Type": "application/json"
@@ -22,6 +13,15 @@ export const apiCall = async <T>(url: string, realm: Realm, params?: { [key: str
     const apiInfo = ApiSourceInfo[apiSource];
 
     const apiEndpoint = `${apiInfo.baseUrl}?apikey=${apiInfo.key()}`;
+
+    const data = {
+      secret: apiInfo.secret(),
+      url,
+      params: {
+        r: realm,
+        ...params,
+      }
+    };
 
     const result = await axios.post<APIResponse<T>>(apiEndpoint, data, config);
     return result.data.response;
